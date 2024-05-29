30995b5349d1e0c6c36d9656ecb53fb369dc2a00
Tim Tattersall <tim@tattsy.com>
2024-05-29 19:54:54 +1000

    format formatting file

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,23 +1,25 @@
 return {
   {
-  'nvimtools/none-ls.nvim',
+    'nvimtools/none-ls.nvim',
     config = function()
-      local null_ls = require('null-ls')
+      local null_ls = require 'null-ls'
 
       null_ls.setup {
         debug = false,
         sources = {
           null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.yapf.with({
-            extra_args = {'--style={based_on_style: pep8, indent_width: 2, column_limit: 120, continuation_indent_width: 2, each_dict_entry_on_separate_line: True}'}
-          }),
+          null_ls.builtins.formatting.yapf.with {
+            extra_args = {
+              '--style={based_on_style: pep8, indent_width: 2, column_limit: 120, continuation_indent_width: 2, each_dict_entry_on_separate_line: True}',
+            },
+          },
           null_ls.builtins.formatting.prettier,
           null_ls.builtins.formatting.terraform_fmt,
-          null_ls.builtins.formatting.spell
-        }
+          null_ls.builtins.formatting.spell,
+        },
       }
 
       vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat buffer' })
-    end
-  }
+    end,
+  },
 }
