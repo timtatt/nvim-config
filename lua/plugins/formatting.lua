3765894b4358c1fbeca2ecb517e219f0d101b363
Tim Tattersall <tim@tattsy.com>
2024-06-05 13:00:29 +1000

    updating formatting options

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,3 +1,12 @@
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format {
+    filter = function(client)
+      -- only use null-ls for formatting
+      return client.name == 'null-ls' or client.name == 'gopls'
+    end,
+    bufnr = bufnr,
+  }
+end
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 return {
@@ -16,7 +25,7 @@ return {
             },
           },
           null_ls.builtins.formatting.shfmt.with {
-            extra_args = {'-i', '2', '-sr', '-ci' }
+            extra_args = { '-i', '2', '-sr', '-ci' },
           },
           null_ls.builtins.formatting.prettierd.with {
             env = {
@@ -25,17 +34,20 @@ return {
           },
           null_ls.builtins.formatting.terraform_fmt,
         },
+        on_attach = function(_, bufnr)
+          vim.api.nvim_clear_autocmds { group = augroup }
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = augroup,
+            callback = function()
+              lsp_formatting(bufnr)
+            end,
+          })
+        end,
       }
 
-      vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat buffer' })
-
-      vim.api.nvim_clear_autocmds { group = augroup }
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = augroup,
-        callback = function()
-          vim.lsp.buf.format()
-        end,
-      })
+      vim.keymap.set('n', '<leader>f', function(bufnr)
+        lsp_formatting(bufnr)
+      end, { desc = '[F]ormat buffer' })
     end,
   },
 }
