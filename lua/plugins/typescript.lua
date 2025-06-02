2127e67130f0c680a81e11a4b8d6ed43bb45d1e7
Tim Tattersall <tim@tattsy.com>
2025-06-02 22:24:51 +1000

    using typescript tools

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,3 +1,8 @@
 return {
   { 'dmmulroy/ts-error-translator.nvim' },
+  {
+    'pmizio/typescript-tools.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    opts = {},
+  },
 }
